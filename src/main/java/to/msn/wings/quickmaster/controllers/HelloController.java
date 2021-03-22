5e482f37c9e6aa387093b89ae19397f8314058b9
Black Fishscale <black.fish.scale@gmail.com>
2021-03-22 21:23:37 +0900

    first chapter completed.

---
+++ src/main/java/to/msn/wings/quickmaster/controllers/HelloController.java
@@ -0,0 +1,40 @@
+package to.msn.wings.quickmaster.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import to.msn.wings.quickmaster.models.BookRepository;
+
+@Controller
+public class HelloController {
+	
+	private BookRepository rep;
+	
+	@Autowired
+	public HelloController(BookRepository rep) {
+		this.rep = rep;
+	}
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String index() {
+		return "こんにちは、世界！";
+	}
+	
+	@GetMapping("/greet")
+	public String greet(Model model) {
+		model.addAttribute("message", "こんにちは、世界！");
+		return "greet";
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("books", rep.findAll());
+		
+		return "list";
+	}
+
+}
